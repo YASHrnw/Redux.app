309da8986c41f6dac05401cb230957bebaf1e05b
YASHrnw <f347yash@gmail.com>
2024-07-01 12:54:12 +0530

    add

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Display from './Redux/Display'
+import Input from './Redux/Input'
+import "bootstrap/dist/css/bootstrap.min.css"
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1>Redux todolist</h1>
+      <Input/>
+      <Display/>
+    </>
+  )
+}
+
+export default App
